eee4c8b0b066378e3a858f864a7f4637a5551fdc
Ferdi van der Werf <efcm@vdwerf.eu>
2013-11-07 09:50:32 +0100

    Set group based on platform family

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,13 @@
 debian_root  = "/etc/ssl"
 debian_certs = "#{debian_root}/certs"
 debian_keys  = "#{debian_root}/private"
-debian_group = "ssl-cert"
+
+case node['platform_family']
+when 'debian', 'ubuntu'
+  debian_group = "ssl-cert"
+when 'rhel'
+  debian_group = "root"
+end
 
 default['ssl']['certs_dir'] = debian_certs
 default['ssl']['keys_dir']  = debian_keys
