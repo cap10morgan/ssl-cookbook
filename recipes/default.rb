0ebb45b27bdaa58012ab88cd71ef3325311270b1
Peter Fern <github@obfusc8.org>
2013-11-18 14:03:30 +1100

    Support deploying CA certificates, including hash symlink.

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,21 +2,24 @@ search(:ssl, '*:*') do |s|
   ssl = Chef::EncryptedDataBagItem.load("ssl", s['id'])
 
   cert_domain = ssl['id'].gsub('_', '.')
-  install_cert(cert_domain, ssl['cert'])
+  install_cert(cert_domain, ssl['cert']) if ssl['cert']
+
+  install_cert(cert_domain, ssl['ca'], :ca => true) if ssl['ca']
 
   chain_certs = ssl['chain']
   if chain_certs
     combined_chain_certs = chain_certs.join("\n")
-    install_cert(cert_domain, combined_chain_certs, :is_chain)
+    install_cert(cert_domain, combined_chain_certs, :chain => true)
   end
 
-  key = ssl['key']
-  key_file = "#{cert_domain}.key"
-  file "#{File.join node['ssl']['keys_dir'], key_file}" do
-    owner 'root'
-    group "#{node['ssl']['group']}"
-    mode '0640'
-    content key
+  if key = ssl['key']
+    key_file = "#{cert_domain}.key"
+    file "#{File.join node['ssl']['keys_dir'], key_file}" do
+      owner 'root'
+      group "#{node['ssl']['group']}"
+      mode '0640'
+      content key
+    end
   end
 
   # this is needed by nginx
